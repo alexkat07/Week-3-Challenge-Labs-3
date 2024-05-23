8817bf957902f2afef9fee68f075c47392729e80
alexk <alexk@DESKTOP-QL7PNT4>
2024-05-23 16:23:07 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Week_3_Challenge_Labs_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Indicies([2, 7, 11, 15], 9));
+
+            static string Indicies(int[] myArray, int target) 
+            {
+                int L = 0;
+                int R = myArray.Length-1;
+                while (L < R) 
+                {
+                    int addition = myArray[L] + myArray[R];
+                    if (addition == target) 
+                    {
+                        return $"[{L},{R}]";
+                    }
+                    else if (addition > target) 
+                    {
+                        R--;
+                    }
+                    else
+                    {
+                        L++;    
+                    }   
+                }
+                return "none";
+            }
+        }
+    }
+}
